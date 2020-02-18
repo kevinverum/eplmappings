b7b88424a01df5e3dc5c55b98152517a6b4a4579
Kevin <kevin@Mac-mini.localdomain>
2020-02-18 15:27:01 +1300

    version 1

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ $mapper = new \kevinverum\eplmappings\EPLMapper();
 
 $json = json_encode(
     [
-        "modTime" => "2019-12-12-10:55:06",
+        "modTime" => "2019-08-12-10:55:06",
         "description" => "This is a house",
         "headline" => "This is a headline",
         "extraFields" => [
@@ -18,32 +18,32 @@ $json = json_encode(
                 "value" => "Residential"
             ]
         ],
-        "agentID" => "TMANA001",
-        "uniqueID" => "M5828",
+        "agentID" => "TMAAAAAAAAAA",
+        "uniqueID" => "M1234AAAAAA",
         "authority" => "exclusive",
         "underOffer" => false,
         "listingAgent" => [
             [
                 "id"=>1,
-                "name" => "Murray Woodley"
+                "name" => "M W"
             ],
             [
                 "id"=>2,
-                "name" => "Susan Woodley"
+                "name" => "S W"
             ]
         ],
-        "priceView" => "Buyer Enquiry From $685,000",
+        "priceView" => "Buyer Enquiry From $444,000",
         "price" => [
             "display" => true,
-            "value"=>685000
+            "value"=>99999
         ],
         "address" => [
             "display" => true,
             "streetNumber"=>13,
-            "street" =>"Colonial Grove",
+            "street" =>"C9",
             "suburb" => [
                 "display"=>true,
-                "value"=>"Tawa"
+                "value"=>"T"
             ],
             "city"=>"Wellington",
             "state"=>"Wellington",
@@ -51,8 +51,8 @@ $json = json_encode(
             "country"=>"NZ"
         ],
         "category"=>"House",
-        "headline"=>"AFFORDABLE TAWA",
-        "description"=>"BEO $685,000&#13;&#10;&#13;&#10;OPEN HOME SUNDAY 15 DECEMBER - CANCELLED &#13;&#10;&#13;&#10;This solid weatherboard clad home, in a sunny location has potential to add value.  &#13;&#10;&#13;&#10;•&#9;3-4 bedrooms, Separate kitchen&#13;&#10;•&#9;Separate dining room with external access to a great decked area&#13;&#10;•&#9;Separate lounge also with external access &#13;&#10;•&#9;Large separate laundry&#13;&#10;•&#9;Refurbished family bathroom&#13;&#10;•&#9;Refurbished ensuite, 2 separate toilets&#13;&#10;•&#9;Downstairs rumpus/4th bedroom&#13;&#10;•&#9;2 separate internal single garages with auto doors. &#13;&#10;•&#9;Elevated, sunny, fully fenced backyard ideal for children &amp; pets &#13;&#10;•&#9;Close to sought after Greenacres primary school &#13;&#10;•&#9;LIM Report available&#13;&#10;•&#9;RV $710,000&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;",
+        "headline"=>"Headline",
+        "description"=>"Description",
 
         "features" => [
             "bedrooms" => 3,
@@ -61,35 +61,35 @@ $json = json_encode(
             "garages" => 2
         ],
 
-        "councilRates"=>"$2,936.70",
+        "councilRates"=>"$2,9999.70",
 
         "landDetails" => [
             "area"=> [
-                "value" => 569,
+                "value" => 777,
                 "unit" => "squareMeter"
             ]
         ],
 
         "buildingDetails" => [
             "area"=> [
-                "value" => 200,
+                "value" => 777,
                 "unit" => "squareMeter"
             ]
         ],
 
         "featureImage" => [
-            "url" => "https://tommys.sgp1.digitaloceanspaces.com/654497_web.jpg",
-            "modTime" => "2019-11-19-15:00:05"
+            "url" => "https://example.com._web.jpg",
+            "modTime" => "2019-08-19-15:00:05"
         ],
 
         "images" => [
             [
-                "url" => "https://tommys.sgp1.digitaloceanspaces.com/654498_web.jpg",
-                "modTime" => "2019-11-19-15:00:05"
+                "url" => "https://example.com._web.jpg",
+                "modTime" => "2019-08-19-15:00:05"
             ],
             [
-                "url" => "https://tommys.sgp1.digitaloceanspaces.com/654499_web.jpg",
-                "modTime" => "2019-11-19-15:00:05"
+                "url" => "https://example.com._web.jpg",
+                "modTime" => "2019-08-19-15:00:05"
             ]
         ],
 
